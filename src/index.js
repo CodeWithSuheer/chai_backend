3152873c29d80d04e7844b29aa4d8153902826a0
Suheer Zahid <150055579+CodeWithSuheer@users.noreply.github.com>
2024-07-14 20:02:33 +0500

    adding utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-const app = express();
-
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server listening on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log('MONGODB Connection failed:', err);
+    })
