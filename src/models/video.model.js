f341fa4ba2c371a48cf252adc6f5f8406b4d8b8b
Suheer Zahid <suheer.work18@gmail.com>
2024-07-19 02:58:22 +0500

    ...

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -2,45 +2,46 @@ import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
-    {
-        videoFile: {
-            type: String,  //cloudinary
-            required: [true, "Video is required"]
-        },
-        thumbnail: {
-            type: String,  //cloudinary
-            required: [true, "Thumbnail is required"]
-        },
-        title: {
-            type: String,
-            required: [true, "Title is required"]
-        },
-        description: {
-            type: String,
-            required: [true, "Description is required"]
-        },
-        duration: {
-            type: Number,
-            required: true
-        },
-        views: {
-            type: Number,
-            default: 0
-        },
-        isPublished: {
-            type: Boolean,
-            default: true
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        },
-        isSeen: {
-            type: Boolean,
-            default: false
-        }
+  {
+    videoFile: {
+      type: String, //cloudinary
+      required: [true, "Video is required"],
     },
-    { timestamps: true });
+    thumbnail: {
+      type: String, //cloudinary
+      required: [true, "Thumbnail is required"],
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    isSeen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
